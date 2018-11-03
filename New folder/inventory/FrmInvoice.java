41e56f3520687c3772f0312aad548c311d483eba
JJ Verzella <jj.verzella@gmail.com>
2018-11-03 09:45:06 -0400

    First commit

---
+++ New folder/inventory/FrmInvoice.java
@@ -0,0 +1,24 @@
+package inventory;
+
+/*
+       WALA NA ADD AnG CONTols SA  JPANEL , KINI JDIALOG , pag himp ug inv form
+*/
+
+import javax.swing.*;
+import utils.InvoiceGenerator;
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+
+public class FrmInvoice extends JInternalFrame
+{
+	
+	public FrmInvoice()
+	{
+            System.out.println("Clicked me in FRM Invoice");
+	}
+
+	// private methods
+
+	/////////////////////////////// end of methods ///////////////////////////////
+}
